c05fd43372c49592951e62e58678f6e3b6aca457
diogotvf7 <diogotvf7@gmail.com>
2024-04-23 00:46:48 +0100

    Some progress on the distribution analysis plots

---
+++ src/distributions.py
@@ -0,0 +1,55 @@
+import seaborn as sns
+
+GRADE = ['LGG','GBM']
+PROJECT = ['TCGA-GBM', 'TCGA-GBM']
+GENDER = ['Male', 'Female']
+RACE = ['White', 'Black or African American', 'Asian', 'American Indian or Alaska Native']
+GENE = ['Not Mutated', 'Mutated']
+
+
+
+def gradePlot(df):
+    gradeCounts = df['Grade'].value_counts()
+    plt.figure()
+    plot = gradeCounts.plot(
+        kind="pie", 
+        x=GRADE, 
+        labels=GRADE, 
+        legend=True, 
+        y='Grade', 
+        figsize=(5, 5)
+    ) 
+    plt.savefig('../plots/grade_distribution.png')
+
+def genderPlot(df, show):
+    genderCounts = df['Gender'].value_counts()
+    plt.figure()
+    plt.pie(genderCounts)
+    plt.legend(True)
+    plt.show()
+    genderCounts.plot(
+        kind="pie", 
+        x=GENDER, 
+        labels=GENDER, 
+        legend=True, 
+        y='Gender', 
+        figsize=(5, 5)
+    )
+    plt.savefig('../plots/gender_distribution.png')
+
+def agePlot(df):
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.hist(df['Age_at_diagnosis'], bins=20, color='skyblue', edgecolor='black')
+    plt.xlabel('Age at Diagnosis')
+    plt.ylabel('Frequency')
+    plt.title('Age at Diagnosis Distribution')
+
+    # Box plot
+    plt.subplot(1, 2, 2)
+    plt.boxplot(df['Age_at_diagnosis'], vert=False)
+    plt.xlabel('Age at Diagnosis')
+    plt.title('Age at Diagnosis Distribution')
+
+    plt.tight_layout()
+    plt.show()
