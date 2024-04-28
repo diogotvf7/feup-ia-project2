6fd00effb54c2cffb69760227fe3ae52c5a5fc61
thePeras <jtopereira@gmail.com>
2024-04-28 23:57:13 +0100

    knn in notebook

--- src/models/knn.py
+++ src/models/knn.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.metrics import classification_report
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 def knn(df):
     X = df.drop('Grade', axis=1)
@@ -13,7 +15,7 @@ def knn(df):
     print(X_train.shape)
     print(X_test.shape)
 
-    model = KNeighborsClassifier(n_neighbors=5, metric='minkowski')
+    model = KNeighborsClassifier(n_neighbors=7, metric='minkowski')
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
@@ -23,8 +25,8 @@ def knn(df):
     cm = confusion_matrix(y_test, y_pred)
     print(cm)
     disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-    disp.plot()
-    plt.show()
 
-    cr = classification_report(y_test, y_pred)
-    print(cr)
+    print(f"Accuracy: {accuracy_score(y_test, y_pred):.2f}")
+
+DATASETS = ['dataset/TCGA_GBM_LGG_Mutations_all.csv', './dataset/TCGA_InfoWithGrade.csv']
+knn(pd.read_csv(DATASETS[1]))
